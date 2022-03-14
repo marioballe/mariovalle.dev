a912f17d2792cd747b624ee96f64abc00741c195
marioballe <ballestercalleja@gmail.com>
2022-03-14 13:14:37 +0100

    add hover efect to buttons

--- marioballe_dev/src/components/LinkTree.vue
+++ marioballe_dev/src/components/LinkTree.vue
@@ -33,6 +33,10 @@ export default {
   font-weight: 600;
   text-decoration: none;
 }
+.btn:hover,
+.btn:focus {
+  text-decoration-color: currentcolor;
+}
 .btn.btn-large {
   font-size: 18px;
   height: auto;
@@ -49,12 +53,22 @@ export default {
   margin: 0px auto;
   height: 5em;
 }
+.button-twitter:hover {
+  color: var(--color-twitter-official);
+  background-color: var(--vt-c-white);
+  border-color: var(--vt-c-white);
+}
 .button-linkedin {
   color: var(--vt-c-white);
   background-color: var(--color-linkedin-official);
   border-color: var(--color-linkedin-official);
   margin: 0px auto;
   height: 5em;
+}
+.button-linkedin:hover {
+  color: var(--color-linkedin-official);
+  background-color: var(--vt-c-white);
+  border-color: var(--vt-c-white);
 }
 .button-twitch {
   color: var(--vt-c-white);
@@ -63,12 +77,22 @@ export default {
   margin: 0px auto;
   height: 5em;
 }
+.button-twitch:hover {
+  color: var(--color-twitch-official);
+  background-color: var(--vt-c-white);
+  border-color: var(--vt-c-white);
+}
 .button-github {
   color: var(--vt-c-white);
   background-color: var(--color-github-official);
   border-color: var(--color-github-official);
   margin: 0px auto;
   height: 5em;
+}
+.button-github:hover {
+  color: var(--color-github-official);
+  background-color: var(--vt-c-white);
+  border-color: var(--vt-c-white);
 }
 .button-discord {
   color: var(--vt-c-white);
@@ -77,6 +101,11 @@ export default {
   margin: 0px auto;
   height: 5em;
 }
+.button-discord:hover {
+  color: var(--color-discord-official);
+  background-color: var(--vt-c-white);
+  border-color: var(--vt-c-white);
+}
 .button-spotify {
   color: var(--vt-c-white);
   background-color: var(--color-spotify-official);
@@ -84,11 +113,21 @@ export default {
   margin: 0px auto;
   height: 5em;
 }
+.button-spotify:hover {
+  color: var(--color-spotify-official);
+  background-color: var(--vt-c-white);
+  border-color: var(--vt-c-white);
+}
 .button-youtube {
   color: var(--vt-c-white);
   background-color: var(--color-youtube-official);
   border-color: var(--color-youtube-official);
   margin: 0px auto;
   height: 5em;
+}
+.button-youtube:hover {
+  color: var(--color-youtube-official);
+  background-color: var(--vt-c-white);
+  border-color: var(--vt-c-white);
 }
 </style>
